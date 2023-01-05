b54c51c5846e9e684bd792788213127ebf7ff368
nathaniel121215 <nathaniel121215@gmail.com>
2023-01-05 16:31:20 +0800

    landing page update

---
+++ react_project_01/src/components/HeroSection.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../App.css';
+import './HeroSection.css';
+
+function HeroSection() {
+    return (
+    <div className='hero-container'>
+        <video src="/videos/video-2.mp4" autoPlay loop muted></video>
+        <div className="main-header">ADVENTURE AWAITS</div>
+        <div className="sub-header">What are you waiting for?</div>
+        <div className="button-container">
+            <button className="btns">GET STARTED</button>
+            <button className="btns btns--outline">WATCH TRAILER</button>
+        </div>
+    </div>
+    );
+  }
+  
+  export default HeroSection;
